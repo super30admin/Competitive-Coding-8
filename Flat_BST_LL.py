0172a4a00d74a99df43dd6d89b28a25b749f99f0
Sachin Devatar <73754947+SachinDevatar77@users.noreply.github.com>
2021-03-01 08:05:16 -0600

    Create Flat_BST_LL.py

---
+++ Flat_BST_LL.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        if root.right:
+            temp = root.right
+            if root.left:
+                root.right = root.left
+                root.left = None
+                node = root
+                while node.right:
+                    node = node.right
+                node.right = temp
+        else:
+            root.right = root.left
+            root.left = None
+        self.flatten(root.right)

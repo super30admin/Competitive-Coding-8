dcf82f0f6374f6ab5d2fdb37247561cab5414181
SuhasMC <chandrashekar.su@husky.neu.edu>
2021-08-07 19:28:57 -0400

    Completed Competitive-Coding-8

---
+++ FlattenBTtoLinkedList.java
@@ -0,0 +1,35 @@
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        //base
+        if(root == null || (root.left == null && root.right == null))
+            return;
+        //logic
+        flatten(root.left);
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = null;
+        while(root.right != null){
+            root = root.right;
+        }
+        root.right = temp;
+        flatten(root.right);
+    }
+}

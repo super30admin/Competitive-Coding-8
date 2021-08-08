fe39efb16f53a1bf3463419c2123e0a7b641c064
palakbaid95 <56458390+palakbaid95@users.noreply.github.com>
2021-08-08 16:28:54 -0700

    Completed Competitive Coding 8

---
+++ MinimumWindowSubstring.java
@@ -0,0 +1,53 @@
+package CompetitiveCoding8;
+
+import java.util.HashMap;
+
+public class MinimumWindowSubstring {
+    /* Created by palak on 8/8/2021 */
+
+    public String minWindow(String s, String t) {
+        if(s.length() == 0 || t.length() == 0) return "";
+
+        HashMap<Character, Integer> tmap = new HashMap<>();
+
+        for(int i = 0;i < t.length() ; i++){
+            char c = t.charAt(i);
+            tmap.put(c, tmap.getOrDefault(c,0)+1);
+        }
+
+        int windowSize = tmap.size();
+        int l = 0;
+        int r = 0;
+        int window =0;
+        int[] res = new int[] {-1,0,0};
+        HashMap<Character,Integer> smap = new HashMap<>();
+
+        while(r < s.length()) {
+            char c = s.charAt(r);
+            smap.put(c, smap.getOrDefault(c,0)+1);
+
+            if(tmap.containsKey(c) && smap.get(c).intValue() == tmap.get(c).intValue())                 {
+                window++;
+            }
+            while(l <= r && window == windowSize){
+                c = s.charAt(l);
+                if(res[0] == -1 || res[0] > r-l+1){
+                    res[0] = r - l + 1;
+                    res[1] = l;
+                    res[2] = r;
+                }
+                smap.put(c, smap.get(c)-1);
+                if(tmap.containsKey(c) && smap.get(c).intValue() < tmap.get(c).intValue()){
+                    window--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return res[0] == -1 ? "" : s.substring(res[1], res[2]+1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

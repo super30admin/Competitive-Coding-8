8ba8501353873e073ee2c84a0da27dd44872cf22
hackerrank <me@hackerrank.com>
2021-10-16 09:35:54 -0700

    Completed Sprint Competitive-Coding-8

---
+++ Flattenbinarytree.java
@@ -0,0 +1,29 @@
+
+time complexity: O(n)
+space complexity: O(n)
+
+//
+
+class Solution {
+    public void flatten(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode p = root;
+ 
+        while(p != null || !stack.empty()){
+ 
+            if(p.right != null){
+                stack.push(p.right);
+            }
+ 
+            if(p.left != null){
+                p.right = p.left;
+                p.left = null;
+            }else if(!stack.empty()){
+                TreeNode temp = stack.pop();
+                p.right=temp;
+            }
+ 
+            p = p.right;
+    }
+}
+}

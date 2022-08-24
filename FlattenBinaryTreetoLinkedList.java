cfb633ce1b24fe36d2eb30eefbc90874be6cd9b1
malavikast <malavikasreedhar0605@gmail.com>
2022-08-23 20:12:09 -0700

    Completed Competitive-Coding-8

---
+++ FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+//Time Complexity: O(n)
+//Space Complexity: O(1) 
+class Solution {
+    public void flatten(TreeNode root) {
+        // base case
+        if (root == null || root.left == null && root.right == null)
+            return;
+        // recursive case
+        if (root.left != null) {
+            flatten(root.left);
+            TreeNode Tempright = root.right;
+            root.right = root.left;
+            root.left = null;
+            while (root.right != null) {
+                root = root.right;
+
+            }
+            root.right = Tempright;
+
+        }
+        flatten(root.right);
+
+    }
+}

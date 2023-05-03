ed8b2d9191c80a0315753631a0b4c8cfca158a28
tirthaj.rikame@gmail.com <tirthaj.rikame@gmail.com>
2023-05-03 00:23:16 -0400

    Completed Competitive Coding-8. Please review.

---
+++ Flatten Binary Tree to Linked List.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        # Time Complexity: O(n)
+        # Space Complexity: O(h)
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+        def dfs(root):
+            if not root:
+                return None
+
+            leftTail = dfs(root.left)
+            rightTail = dfs(root.right)
+
+            if root.left:
+                leftTail.right = root.right
+                root.right = root.left
+                root.left = None
+            last = rightTail or leftTail or root
+            return last
+
+        dfs(root)
+
+
+
+
+
+
+
